21bddc297e6e991b88d360461890b288d74cf0cc
Emily Olszewski <92195356+emilyoski@users.noreply.github.com>
2023-10-31 17:42:43 -0700

    split functionality into config and client

--- src/sdk/SmoothSailClient.js
+++ src/sdk/SmoothSailClient.js
@@ -1,2 +1,72 @@
-// connection functionality implemented separately
-// initial request for data, SSE connection, polling?
+import "dotenv/config";
+import EventSource from "eventsource";
+// import { TEST_FLAG_1 } from "../data/testFlags";
+import { Flag } from "./classes/Flag";
+
+export class SmoothSailClient {
+  constructor(config) {
+    this.flagData = {};
+    this.config = config;
+
+    // needed?
+    // this.last_updated;
+    // this.updatedLastUpdated();
+  }
+
+  async fetchFeatureFlags() {
+    try {
+      // can pass in with initial headers
+      // const headers = {
+      //   headers: { Authorization: this.config.sdkKey },
+      // };
+      const response = await fetch(this.config.devAddress);
+      const data = await response.json();
+      this.setFlags(data.payload);
+      console.log("flag data", this.flagData);
+
+      // with test data
+      // this.setFlags(TEST_FLAG_1.payload);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  setFlags(flags) {
+    this.flagData = {};
+    for (let flag in flags) {
+      this.flagData[flag] = new Flag(flags[flag]);
+    }
+  }
+
+  evaluateFlag(flagKey, userContext) {
+    const flag = this.flagData[flagKey];
+    return flag && flag.evaluateFlag(userContext);
+  }
+
+  updatedLastUpdated() {
+    // is this still needed?
+  }
+
+  openSSEConnection() {
+    // sdkKey can be used as query parameter for eventsource?
+    // sdkKey used as event type?
+    const eventSource = new EventSource(process.env.SSE_ENDPOINT);
+
+    eventSource.onopen = () => {
+      console.log(`connection to ${process.env.SSE_ENDPOINT} opened!`);
+    };
+
+    eventSource.onmessage = (e) => {
+      const notification = JSON.parse(e.data);
+
+      if (notification.type === "flags") {
+        console.log(notification);
+        this.setFlags(notification);
+      }
+    };
+
+    eventSource.onerror = (error) => {
+      console.error("SSE error:", error);
+    };
+  }
+}
---
+++ src/sdk/SmoothSailConfig.js
@@ -0,0 +1,22 @@
+import { SmoothSailClient } from "./SmoothSailClient";
+
+export class SmoothSailConfig {
+  constructor(sdkKey, bearerAddress) {
+    this.sdkKey = sdkKey;
+    // authenticate here?
+    this.bearerAddress = bearerAddress;
+    this.devAddress = "http://localhost:3001/api/flags";
+  }
+
+  async connect() {
+    const client = new SmoothSailClient(this);
+    try {
+      await client.fetchFeatureFlags();
+      await client.openSSEConnection();
+      return client;
+    } catch (error) {
+      console.log("SmoothSail Server Client: connection failed");
+      return client;
+    }
+  }
+}
